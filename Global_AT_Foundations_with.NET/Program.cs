ab23295eae34883cc7945b8d660f9a671823cc59
Andrei Kramski <Andrei_Kramski@epam.com>
2022-10-17 15:20:56 +0200

    Добавьте файлы проекта.

---
+++ Global_AT_Foundations_with.NET/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Task1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value:");
+        var strintArray = Console.ReadLine();                        
+        int counter = 0;                                            
+
+        for (int i = 0; i < strintArray.Length - 1; i++)          
+        {
+            if (strintArray[i] != strintArray[i + 1])           
+            {
+                counter++;                                    
+            }
+        }
+        Console.WriteLine("You have " + counter + " unequal characters");    
+    }
+}
